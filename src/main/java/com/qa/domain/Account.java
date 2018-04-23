100073cd111efc28b80358b35c613032edcc0cdb
Admin <Admin@STUDENT31>
2018-04-23 16:57:54 +0100

    "Added import.sql file"

--- src/main/java/com/qa/domain/Account.java
+++ src/main/java/com/qa/domain/Account.java
@@ -1,11 +1,24 @@
 package com.qa.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 
+	@Id @GeneratedValue
+	private Long id;
+	@Column(length = 10)
 	private String firstName;
+	@Column(length = 10)
 	private String secondName;
+	@Column(length = 4)
 	private String accountNumber;
 
+	
 	public Account(String firstName, String secondName, String accountNumber) {
 		this.firstName = firstName;
 		this.secondName = secondName;
@@ -36,4 +49,11 @@ public class Account {
 		this.accountNumber = accountNumber;
 	}
 
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + ", accountNumber="
+				+ accountNumber + "]";
+	}
+
+	
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Account (id, firstName, secondName, accountNumber) values (1, 'John', 'Doe', '1234');
+insert into Account (id, firstName, secondName, accountNumber) values (2, 'Jane', 'Doe', '1235');
+insert into Account (id, firstName, secondName, accountNumber) values (3, 'Jim', 'Taylor', '1236');

1f5eae9d6c98f59e84c4a07ce63cbb5d7613c18e
Admin <Admin@STUDENT31>
2018-04-24 11:32:41 +0100

    "Added CRUD Functions"

---
+++ src/main/java/com/qa/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+package com.qa.repository;
+
+
+public interface AccountRepository {
+
+	String getAllAccounts();
+
+	String createAccount(String accout);
+
+	String updateAccount(Long id, String accountToUpdate);
+
+	String deleteAccount(Long id);
+
+}
+
+
